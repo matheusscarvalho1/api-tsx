e18be11bb8554412a6783a75fef1be31f278742a
Matheus de Souza Carvalho <matheus.carvalho@ginco.local>
2024-02-07 12:53:15 -0400

    feat: Adicionando API de Delete e finalizando o projeto

--- src/routes/transactionsRoutes.ts
+++ src/routes/transactionsRoutes.ts
@@ -20,13 +20,13 @@ export default async function transactionsRoutes (app: FastifyInstance) {
       const { name, amount } = req.body as TransactionData;
 
        try {
-        const transaction = await prisma.transactions.create({
+        const newTransaction = await prisma.transactions.create({
           data: {
               name,
               amount,
           }
         })
-        reply.code(201).send(transaction);
+        reply.code(201).send(newTransaction);
 
        } catch (error) {
         console.error('Erro ao criar transação:', error);
@@ -35,41 +35,83 @@ export default async function transactionsRoutes (app: FastifyInstance) {
       });
 
     //Seção de transações - Listar todas transações
-    app.get('/', async () => {
-      const transaction = await prisma.transactions.findMany();
+    app.get('/', async (req, reply) => {
+      const listTransactions = await prisma.transactions.findMany();
 
-      return {
-        transaction,
-      }
+      reply.code(200).send(listTransactions)
       });
 
     //Seção de transações - Listar todas transações por ID
-    app.get('/:id', async (req) => {
+    app.get('/:id', async (req, reply) => {
       const params = req.params as RequestParams;
-      const transaction = await prisma.transactions.findUnique({
+      const listTransactionById = await prisma.transactions.findUnique({
         where: {
           id: String(params.id),
         }
       });
 
-      return {
-        transaction
-      }
+      reply.code(200).send(listTransactionById);
     });
 
 
     //Seção de transações - Atualizar transações
-    app.put('/', async () => {
-        
+    app.put('/:id', async (req, reply) => {
+        const { name, amount } = req.body as TransactionData;
+        const params = req.params as RequestParams;
+
+        try {
+          const updateTransaction = await prisma.transactions.update({
+            where: {
+              id: String(params.id),
+            },
+            data: {
+              name,
+              amount,
+            }
+          })
+          reply.code(200).send(updateTransaction);
+        } catch (error) {
+          console.error('Erro ao atualizar transação:', error);
+          reply.code(500).send();
+        } 
       });
 
     //Seção de transações - Atualizar transações - Parcialmente
-    app.patch('/', async () => {
-        
-      });
+    app.patch('/:id', async (req,reply) => {
+      const { amount } = req.body as TransactionData;
+      const params = req.params as RequestParams;
+    
+      try {
+        const updateTransactionUnique = await prisma.transactions.updateMany({
+          where: {
+            id: String(params.id),
+          },
+          data: {
+            name: 'Nova transação',
+            amount,
+          }
+        });
+        reply.send(200).send(updateTransactionUnique)
+      } catch (error) {
+        console.error('Erro ao atualizar transação:', error);
+        reply.code(500).send();
+      }
+    })
 
     //Seção de transações - Deletar transações
-    app.delete('/', async () => {
+    app.delete('/:id', async (req, reply) => {
+      const params = req.params as RequestParams;
+      try {
+        await prisma.transactions.delete({
+          where: {
+            id: String(params.id),
+          }
+        })
+        reply.code(200).send('Transação apagada!')
+      } catch (error) {
+        console.error('Erro ao deletar transação:', error);
+        reply.code(500).send();
+      }
         
     });
 }
