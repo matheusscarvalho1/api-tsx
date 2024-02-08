b486b96186c3bfb489330f29bf912debb9d8de9a
Matheus de Souza Carvalho <matheus.carvalho@ginco.local>
2024-02-08 17:27:37 -0400

    Ajustando testes, dado erro

---
+++ src/test/transactions.test.ts
@@ -0,0 +1,52 @@
+import { test, beforeAll, afterAll, describe, expect } from 'vitest'
+import request  from 'supertest'
+import { app } from '../app'
+
+
+describe('Testes de transações', () => {
+    
+    beforeAll(async () => {
+        await app.ready()
+    })
+    
+    afterAll(async () => {
+        await app.close()
+    })
+    
+    test('O usuário deve conseguir criar uma nova transação', async () => {
+         await request(app.server)
+        .post('/transactions')
+        .send({
+            name: 'New transaction',
+            amount: 5000,
+        })
+        .expect(201)
+    })
+
+    test('O usuário deve listar todas as transações', async () => {
+        const createTransactionResponse = await request(app.server)
+        .post('/transactions')
+        .send({
+            name: 'New transaction',
+            amount: 5000,
+        })
+
+        const cookies = createTransactionResponse.get('Set-Cookie')
+
+        const listTransactionsResponse = await request(app.server)
+            .get('/transactions')
+            .set('Cookie', cookies)
+            .expect(200)
+
+            expect(listTransactionsResponse.body.transactions).toEqual([
+               
+                expect.objectContaining({
+                    name: 'New transaction',
+                    amount: 5000,
+                })
+
+            ])
+        
+    })
+
+})
